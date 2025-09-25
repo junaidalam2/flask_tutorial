fe550c2651831fd46c68c68cb3c737748549bcfd
junaidalam2 <junaid.alam2@gmail.com>
2025-09-25 14:07:40 -0700

    completed dynamic data lesson

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+          'id': 1,
+          'title': 'Data Analyst',
+          'location': 'San Francisco, California',
+          'salary': '120, 000'
+    },
+    {
+          'id': 2,
+          'title': 'Data Scientist',
+          'location': 'San Francisco, California',
+          'salary': '190, 000'
+    },
+    {
+          'id': 3,
+          'title': 'Junior Software Engineer',
+          'location': 'New York, New York',
+          'salary': '160, 000'
+    },
+    {
+          'id': 4,
+          'title': 'Software Architect Engineer',
+          'location': 'Houston, Texas',
+    },
+]
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, world!!!</p>"
+    return render_template('home.html', jobs=JOBS)
 
 if __name__ == "__main__":
         app.run(host='0.0.0.0', debug=True)
