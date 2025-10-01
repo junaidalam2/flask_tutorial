e81d4b3281220c6644ae4da4e1805a97117ef06a
junaidalam2 <junaid.alam2@gmail.com>
2025-09-30 23:19:38 -0700

    configuring to cockroach db

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from config import Config
+from extensions import db, migrate
+from dotenv import load_dotenv
+load_dotenv()
 
-db = SQLAlchemy()
-migrate = Migrate()
+import models 
+
 
 
 def create_app():
@@ -14,38 +15,54 @@ def create_app():
       db.init_app(app)
       migrate.init_app(app, db)
 
-      JOBS = [
-      {
-            'id': 1,
-            'title': 'Data Analyst',
-            'salary': '120, 000'
-      },
-      {
-            'id': 2,
-            'title': 'Data Scientist',
-            'location': 'San Francisco, California',
-            'salary': '190, 000'
-      },
-      {
-            'id': 3,
-            'title': 'Junior Software Engineer',
-            'location': 'New York, New York',
-            'salary': '160, 000'
-      },
-      {
-            'id': 4,
-            'title': 'Software Architect Engineer',
-            'location': 'Houston, Texas',
-      },
-      ]
+      Job = models.Job 
+
+      # JOBS = [
+      # {
+      #       'id': 1,
+      #       'title': 'Data Analyst',
+      #       'salary': '120, 000'
+      # },
+      # {
+      #       'id': 2,
+      #       'title': 'Data Scientist',
+      #       'location': 'San Francisco, California',
+      #       'salary': '190, 000'
+      # },
+      # {
+      #       'id': 3,
+      #       'title': 'Junior Software Engineer',
+      #       'location': 'New York, New York',
+      #       'salary': '160, 000'
+      # },
+      # {
+      #       'id': 4,
+      #       'title': 'Software Architect Engineer',
+      #       'location': 'Houston, Texas',
+      # },
+      # ]
 
       @app.route("/")
       def hello_world():
-            return render_template('home.html', jobs=JOBS)
+            jobs = Job.query.all()
+            return render_template('home.html', jobs=jobs)
 
-      @app.route('/api/jobs')
+      @app.route("/api/jobs")
       def list_jobs():
-            return jsonify(JOBS)
+            jobs = Job.query.all()
+            jobs_list = [
+                  {
+                  "id": job.id,
+                  "title": job.title,
+                  "location": job.location,
+                  "salary": job.salary,
+                  } for job in jobs
+            ]
+            return jsonify(jobs_list)
+
+
+      return app
+
 
 if __name__ == "__main__":
         app = create_app()
