cc8471637c910af6e545987879b3a78cc6089f90
junaidalam2 <junaid.alam2@gmail.com>
2025-10-05 18:22:01 -0700

    updated to render db data and updated buttons

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from dotenv import load_dotenv
 load_dotenv()
-import os
-print("DATABASE_URL =", os.getenv("DATABASE_URL"))
 from flask import Flask, render_template, jsonify
 from config import Config
 from extensions import db, migrate
